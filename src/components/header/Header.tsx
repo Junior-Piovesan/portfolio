f4283caeee6ad7255e06435a665b3e8592619056
Junior Piovesan Silva <juniorpiovesan3022@outlook.com>
2023-09-21 13:17:20 -0300

    alterado nome de botão

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -15,7 +15,7 @@ export default function Header() {
           className={ styles.button }
           onClick={ () => navigate('/') }
         >
-          Home
+          Inicio
         </button>
 
         <button
