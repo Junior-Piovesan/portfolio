750f9db726ebe02a3a608960a9f458c05f2be3f4
Junior Piovesan Silva <juniorpiovesan3022@outlook.com>
2023-09-26 16:20:10 -0300

    corrigido erro de build2

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -5,8 +5,10 @@ import roquetIcon from '../../assets/foguete.png';
 export default function Logo() {
   return (
     <div className={ styles.titleContainer }>
+
       <img className={ styles.imgTitle } src={ roquetIcon } alt="Icone de foguete" />
       <h2 className={ styles.title }>JR Dev</h2>
+
     </div>
   );
 }
