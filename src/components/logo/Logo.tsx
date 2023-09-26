e834493510e20070416fa55669b143cea9753ffc
Junior Piovesan Silva <juniorpiovesan3022@outlook.com>
2023-09-26 16:16:34 -0300

    alteração final no logo

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -6,10 +6,9 @@ type PropsType = {
   isCircle:boolean
 };
 
-export default function Logo({ isCircle }:PropsType) {
+export default function Logo() {
   return (
     <div className={ styles.titleContainer }>
-      <div className={ isCircle ? styles.circle : styles.notCircle } />
       <img className={ styles.imgTitle } src={ roquetIcon } alt="Icone de foguete" />
       <h2 className={ styles.title }>JR Dev</h2>
     </div>
